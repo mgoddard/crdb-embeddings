590c515d97362cc8766b5ebc2b10a7e804149c2b
mgoddard <mgoddard@cockroachlabs.com>
2024-02-24 19:04:27 -0500

    Updating to support indexing multiple docs

--- index_doc.py
+++ index_doc.py
@@ -6,8 +6,8 @@ import sys, os, re
 # FIXME: get URL from environment
 url = "http://localhost:1963/index"
 
-if len(sys.argv) != 2:
-  print("Usage: {} file_to_index\n".format(sys.argv[0]))
+if len(sys.argv) < 2:
+  print("Usage: {} file_to_index [file2 ...]\n".format(sys.argv[0]))
   sys.exit(1)
 
 def read_file(in_file):
@@ -17,11 +17,10 @@ def read_file(in_file):
       rv += line
   return rv
 
-doc_uri = sys.argv[1]
-doc_text = read_file(doc_uri)
-doc_uri = re.sub(r"^[\./]+", '', doc_uri)
-#print("URI: {}\nTEXT: {}".format(doc_uri, doc_text))
-
-req = requests.post(url, json = { "uri": doc_uri, "text": doc_text })
-print("Status: {}".format("SUCCESS" if req.status_code == 200 else "FAILED"))
+for doc_uri in sys.argv[1:]:
+  doc_text = read_file(doc_uri)
+  doc_uri = re.sub(r"^[\./]+", '', doc_uri)
+  #print("URI: {}\nTEXT: {}".format(doc_uri, doc_text))
+  req = requests.post(url, json = { "uri": doc_uri, "text": doc_text })
+  print("{}: {}".format(doc_uri, "SUCCESS" if req.status_code == 200 else "FAILED"))
 
