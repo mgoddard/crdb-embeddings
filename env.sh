acc889b3bcdfbfadb9753311b303cd99a66e58f1
mgoddard <mgoddard@cockroachlabs.com>
2024-07-30 06:51:03 -0400

    Env changes and more data

--- env.sh
+++ env.sh
@@ -4,5 +4,6 @@ export LOG_LEVEL=WARN
 export N_THREADS=1
 export CACHE_SIZE=1024
 # For client
-export MAX_RESULTS=3
-export RERANK=regex
+export MAX_RESULTS=4
+#export RERANK=regex
+export RERANK=none
