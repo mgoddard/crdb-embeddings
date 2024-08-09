314b5b7f1d0306d4df35cd09536aae4ccc6c1850
mgoddard <mgoddard@cockroachlabs.com>
2024-08-09 12:28:46 -0400

    Better UX for scaling script

--- k8s/scale_crdb-embeddings.sh
+++ k8s/scale_crdb-embeddings.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-kubectl scale --replicas=4 deployment crdb-embeddings
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: $0 new_replica_count"
+  exit 1
+fi
+
+n_rep=$1
+
+kubectl scale --replicas=$n_rep deployment crdb-embeddings
 
