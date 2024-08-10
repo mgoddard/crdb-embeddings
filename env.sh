d0c9fd7531b7f0e0b881ff672357107f3347736f
mgoddard <mgoddard@cockroachlabs.com>
2024-08-10 11:58:37 -0400

    Eliminating rerank option

--- env.sh
+++ env.sh
@@ -1,9 +1,12 @@
+# For server
 export FLASK_PORT=1972
 export FLASK_HOST=localhost
-export LOG_LEVEL=WARN
+
+export LOG_LEVEL=INFO
 export N_THREADS=1
 export CACHE_SIZE=1024
+export MIN_SENTENCE_LEN=8
+
 # For client
-export MAX_RESULTS=4
-#export RERANK=regex
-export RERANK=none
+export MAX_RESULTS=5
+
