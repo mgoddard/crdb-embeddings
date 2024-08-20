da1ca46651c2565296144898245ed0364cc9a207
mgoddard <mgoddard@cockroachlabs.com>
2024-08-20 08:08:02 -0400

    Older experimental code

--- parallel_index.sh
+++ parallel_index.sh
@@ -5,10 +5,20 @@
 #./index_doc.py $( ./gen_m_of_n.sh 1 2 ./data/*.txt ) &
 
 # 4 parallel procs
-./index_doc.py $( ./gen_m_of_n.sh 0 4 ./data/*.txt ) &
-./index_doc.py $( ./gen_m_of_n.sh 1 4 ./data/*.txt ) &
-./index_doc.py $( ./gen_m_of_n.sh 2 4 ./data/*.txt ) &
-./index_doc.py $( ./gen_m_of_n.sh 3 4 ./data/*.txt ) &
+#./index_doc.py $( ./gen_m_of_n.sh 0 4 ./data/*.txt ) &
+#./index_doc.py $( ./gen_m_of_n.sh 1 4 ./data/*.txt ) &
+#./index_doc.py $( ./gen_m_of_n.sh 2 4 ./data/*.txt ) &
+#./index_doc.py $( ./gen_m_of_n.sh 3 4 ./data/*.txt ) &
+
+# 8 parallel procs
+./index_doc.py $( ./gen_m_of_n.sh 0 8 ./data/*.txt ) &
+./index_doc.py $( ./gen_m_of_n.sh 1 8 ./data/*.txt ) &
+./index_doc.py $( ./gen_m_of_n.sh 2 8 ./data/*.txt ) &
+./index_doc.py $( ./gen_m_of_n.sh 3 8 ./data/*.txt ) &
+./index_doc.py $( ./gen_m_of_n.sh 4 8 ./data/*.txt ) &
+./index_doc.py $( ./gen_m_of_n.sh 5 8 ./data/*.txt ) &
+./index_doc.py $( ./gen_m_of_n.sh 6 8 ./data/*.txt ) &
+./index_doc.py $( ./gen_m_of_n.sh 7 8 ./data/*.txt ) &
 
 wait
 
