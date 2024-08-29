52f0bf2b0bb7f1a402213824c76861359fde7c76
mgoddard <mgoddard@cockroachlabs.com>
2024-08-29 14:57:39 -0400

    Adjustments to KMeans model build params

--- env.sh
+++ env.sh
@@ -8,14 +8,14 @@ export DB_URL="postgres://test_role:123abc@127.0.0.1:26257/defaultdb?sslmode=req
 
 export LOG_LEVEL=INFO
 export N_THREADS=10
-export MIN_SENTENCE_LEN=8
-export N_CLUSTERS=1000
+export MIN_SENTENCE_LEN=12
+export N_CLUSTERS=1024
 export TRAIN_FRACTION=0.5
 export MODEL_FILE=/tmp/model.pkl
 export MODEL_FILE_URL="https://storage.googleapis.com/crl-goddard-text/model_Fastembed_1k.pkl"
-export BATCH_SIZE=768
-export KMEANS_VERBOSE=1
-export KMEANS_MAX_ITER=250
+export BATCH_SIZE=1024
+export KMEANS_VERBOSE=2
+export KMEANS_MAX_ITER=100
 export SKIP_KMEANS=False
 export SECRET="TextWithNoSpecialChars"
 export BLOB_STORE_KEEP_N_ROWS=3
