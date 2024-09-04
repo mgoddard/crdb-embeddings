ce21caec9c1a1cdf191e4a0f7e073ca62f49675a
mgoddard <mgoddard@cockroachlabs.com>
2024-09-04 15:00:18 -0400

    Adding memory limit for container

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
 # For server
-#export FLASK_PORT=1972
-export FLASK_PORT=1999
+export FLASK_PORT=1972
 export FLASK_HOST=localhost
 #export FLASK_PORT=80
 #export FLASK_HOST=vector.la-cucaracha.net
@@ -9,8 +8,8 @@ export DB_URL="postgres://test_role:123abc@127.0.0.1:26257/defaultdb?sslmode=req
 export LOG_LEVEL=INFO
 export N_THREADS=10
 export MIN_SENTENCE_LEN=8
-export N_CLUSTERS=384
-export TRAIN_FRACTION=1.0
+export N_CLUSTERS=4992
+export TRAIN_FRACTION=0.10
 export MODEL_FILE=/tmp/model.pkl
 export MODEL_FILE_URL="https://storage.googleapis.com/crl-goddard-text/model_Fastembed_1536.pkl"
 export BATCH_SIZE=1024
@@ -19,6 +18,7 @@ export KMEANS_MAX_ITER=100
 export SECRET="TextWithNoSpecialChars"
 export BLOB_STORE_KEEP_N_ROWS=3
 export TOKENIZERS_PARALLELISM=false
+export MEMORY_LIMIT_MB=4096
 export SKIP_KMEANS=false
 
 # For client
